ed04be6de76094bcf236413f16f559e97e5ebe30
lmanganyi023 <lmanganyi023@student.wethinkcode.co.za>
2025-01-13 01:06:16 +0200

    designing my database schema

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,8 +3,25 @@ from flask_sqlalchemy  import SQLAlchemy
 
 db = SQLAlchemy()
 
-class ExampleModel(db.Model):
-    id = db.column(db.Integer, primary_key = True)
-    name = db.column(db.String(80), nullable = False)
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, default = db.func.current_timestamp())
+    
+class Files(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_id'), nullable=False)
+    file_name = db.Column(db.String,nullable=False)
+    file_path = db.Column(db.String, nullable=False)
+    uploaded_at = db.Column(db.DateTime, default = db.func.current_timestamp())
+    
+class DataInsights(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    file_id = db.column(db.Integer, db.ForeignKey('file_id'), nullable=False)
+    insights_type = db.Column(db.String(80), nullable=True)
+    insights_data = db.Column(db.String(80), nullable=True)
+    created_at = db.Column(db.DateTime, default = db.func.current_timestamp())
     
     
